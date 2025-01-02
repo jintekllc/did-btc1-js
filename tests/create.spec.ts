eff21dc9235952fe66a399a174133154955ec2f7
Bryan <doesntwork@localhost>
2025-01-02 18:29:02 -0500

    Init btc1-js repo

--- tests/create.spec.ts
+++ tests/create.spec.ts
@@ -1,6 +1,32 @@
 import { DidBtc1 } from '../src/did-btc1.js';
-const deterministic = await DidBtc1.create();
-console.log('deterministic', deterministic);
 
-const sidecar = await DidBtc1.create({ options: { type: 'sidecar' } });
-console.log('sidecar', sidecar);
+describe('applicant = new DcxApplicant({ config: applicantConfig })', () => {
+
+    /**
+     * @description Create a new did and did document using default settings (deterministic)
+     */
+    describe('DidBtc1.create()', async () => {
+        const response = await DidBtc1.create();
+        // Check response property "did"
+        it('should contain property "did" as a string', () => {
+            expect(response).to.have.property('did').that.is.a('string');
+        });
+
+        // Validate the did format
+        it('should contain property "did" as a string', () => {
+            expect(applicant.status).to.have.property('initialized').that.is.a('boolean').and.to.be.false;
+        });
+
+        // Check response property "didDocument"
+        it('should contain property "setup" as a boolean equal to false', () => {
+            expect(applicant.status).to.have.property('setup').that.is.a('boolean').and.to.be.false;
+        });
+        
+        // Check response property "mnemonic"
+    });
+
+    console.log('deterministic', deterministic);
+
+    const sidecar = await DidBtc1.create({ options: { type: 'sidecar' } });
+    console.log('sidecar', sidecar);
+});
